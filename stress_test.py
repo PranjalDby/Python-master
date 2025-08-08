22822e972447aff0e708fa8ef3662efe4ec68e78
pranjaldby <dpranjal.088@gmail.com>
2025-08-08 19:47:29 +0530

    new reddit api added

--- stress_test.py
+++ stress_test.py
@@ -1,24 +1,24 @@
 import multiprocessing
+import random
 import sys
-import threading
 import time
 from concurrent.futures import ThreadPoolExecutor
+
 import numpy as np
-import random
-def cpu_heater(mat1,mat2):
+
+
+def cpu_heater(dimensions):
+    rand_generator = np.random.MT19937(int(time.time()))
     print("Matrix Multiplication......")
-    res = np.matmul(mat1,mat2);
+    for i in range(50):
+        mat1 = rand_generator.random_raw(dimensions)
+        mat2 = rand_generator.random_raw(dimensions)
+        res = np.matmul(mat1, mat2)
     return res
 
-def cross_product(dimensions):
-    rand_generator = np.random.MT19937(int(time.time()))
-
-    for i in range(1,10):
-        matrix = rand_generator.random_raw((10000,1000))
-        matrix2 = rand_generator.random_raw((1000,10000))
-        executor = ThreadPoolExecutor(i,"MAT_MUL")
-        res = executor.submit(cpu_heater,matrix,matrix2)
-        print(res.result())
+def cross_product(dimensions: list[int]):
+    with multiprocessing.Pool() as p:
+        p.map(cpu_heater, [dimensions])
     
     print("Call Complete")
 
@@ -27,8 +27,7 @@ if __name__ == "__main__":
     print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> SYSTEM INFO <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
     print("Platform:{}".format(sys.platform))
     start_time = time.time()
-    cross_product((1000,1000))
+    cross_product([1000,1000])
     duration = time.time() - start_time
     print(f"Duration {duration} seconds")
     print("Hello World")
-
